0eb93ec1a5c0686ea2dc433ff5ed7f1a0513fab7
evgeniy.voronkin <evgeniy.voronkin@gorparkovki.ru>
2019-10-17 19:15:06 +0300

    first commit - selenide 5.4.0 (browser does not close after test execution)

---
+++ src/test/java/TestSimple.java
@@ -0,0 +1,15 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestSimple {
+
+  @Test
+  void initTest(){
+    Configuration.browser = WebDriverRunner.CHROME;
+    open("https://www.google.com/");
+  }
+
+}
